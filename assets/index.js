bd8dcea9c617d941cac8ef6a59442af3b996b9ad
gautham elango <gg@gghost.local>
2017-02-05 19:34:19 +0900

    d

--- assets/index.js
+++ assets/index.js
@@ -4,23 +4,23 @@ var nes;
                 'ui': $('#emulator').JSNESUI({
                     "Working": [
                         //['Bubble Bobble', 'jsnes/roms/Bubble Bobble (U).nes'],
-                        ['Konami Contra 1988', 'https://s3.amazonaws.com/nescat/roms/Contra.nes'],
-                        ['Donkey Kong 1981', 'https://s3.amazonaws.com/nescat/roms/Donkey.nes'],
-                        ['Dr. Mario', 'https://s3.amazonaws.com/nescat/roms/DrMario.nes'],
-                        ['Golf', 'https://s3.amazonaws.com/nescat/roms/Golf.nes'],
-                        ['The Legend of Zelda', 'https://s3.amazonaws.com/nescat/roms/Zelda.nes'],
-                        ['Lemmings', 'https://s3.amazonaws.com/nescat/roms/Lemmings.nes'],
-                        ['Lifeforce', 'https://s3.amazonaws.com/nescat/roms/Lifeforce.nes'],                        
-                        ['Mario Bros 1983', 'https://s3.amazonaws.com/nescat/roms/MarioBros.nes'],
-                        ['Mega Man', 'https://s3.amazonaws.com/nescat/roms/MegaMan.nes'],
-                        ['Pac-Man', 'https://s3.amazonaws.com/nescat/roms/Pac-Man.nes'],
-                        ['Super Mario Bros 1985', 'https://s3.amazonaws.com/nescat/roms/SuperMarioBros.nes'],
-                        ['Tennis', 'https://s3.amazonaws.com/nescat/roms/Tennis.nes'],
+                        ['Konami Contra 1988', 'assets/roms/Contra.nes'],
+                        ['Donkey Kong 1981', 'assets/roms/Donkey.nes'],
+                        ['Dr. Mario', 'assets/roms/DrMario.nes'],
+                        ['Golf', 'assets/roms/Golf.nes'],
+                        ['The Legend of Zelda', 'assets/roms/Zelda.nes'],
+                        ['Lemmings', 'assets/roms/Lemmings.nes'],
+                        ['Lifeforce', 'assets/roms/Lifeforce.nes'],                        
+                        ['Mario Bros 1983', 'assets/roms/MarioBros.nes'],
+                        ['Mega Man', 'assets/roms/MegaMan.nes'],
+                        ['Pac-Man', 'assets/roms/Pac-Man.nes'],
+                        ['Super Mario Bros 1985', 'assets/roms/SuperMarioBros.nes'],
+                        ['Tennis', 'assets/roms/Tennis.nes'],
                         //['Tetris', 'jsnes/roms/Tetris (U) [!].nes'],
-                        ['Concentration Room', 'https://s3.amazonaws.com/nescat/roms/croom/croom.nes'],
-                        ['LJ65', 'https://s3.amazonaws.com/nescat/roms/lj65/lj65.nes'],
-                        ['Tetris 2 1987', 'https://s3.amazonaws.com/nescat/roms/Tetris2.nes'],
-                        ['Zelda II - The Adventure of Link', 'https://s3.amazonaws.com/nescat/roms/Zelda2.nes']
+                        ['Concentration Room', 'assets/roms/croom/croom.nes'],
+                        ['LJ65', 'assets/roms/lj65/lj65.nes'],
+                        ['Tetris 2 1987', 'assets/roms/Tetris2.nes'],
+                        ['Zelda II - The Adventure of Link', 'assets/roms/Zelda2.nes']
                     ]
 
                 })
