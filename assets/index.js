1608714074ac8c1b6e547f2541ab08188c5397f7
gautham elango <gautham@gcubed.co>
2017-03-04 21:54:24 +0900

    changes

--- assets/index.js
+++ assets/index.js
@@ -25,67 +25,6 @@ $(function() {
     });
 });
 
-if (window.location.href == "/web/20161106234311/http://www.nescat.co/" || window.location.href == "/web/20161106234311/http://nescat.co/" || window.location.href == "/web/20161106234311/http://nescat.net/" ) {
-          window.location.href = "/web/20161106234311/http://www.nescat.net";
+if (window.location.href == "http://nescat.gcubed.co/") {
+          window.location.href = "https://nescat.gcubed.co/";
 }
-
-if (window.location.href == "/web/20161106234311/https://nescat.net/") {
-    window.location.href = "/web/20161106234311/https://www.nescat.net";
-}
-var zoombtn = document.getElementByClass("#zoombtn");
-        // Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-var modal1 = document.getElementById('myModal1');
-
-// Get the button that opens the modal
-var btn1 = document.getElementById("myBtn1");
-
-// Get the <span> element that closes the modal
-var span1 = document.getElementsByClassName("close1")[0];
-
-// When the user clicks the button, open the modal
-btn1.onclick = function() {
-    modal1.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span1.onclick = function() {
-    modal1.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    }
-}
-
-function reloadFunction() {
-  window.location.reload(false); 
-}
-
-$('.nes-zoom').remove();
